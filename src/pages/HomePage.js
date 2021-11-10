b6c13bc04e9ae1f2737f720d7ac39da3b420371d
siwon <ksw3721@gmail.com>
2021-11-10 09:43:24 +0900

    created project with react app & did basic settings

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className='homepage'>
+    <div className='directoryMenu'>
+      <div className='menuItem'>
+        <div className='content'>
+          <h1 className='title'>hat</h1>
+          <span className='subtitle'>explanation</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
